1aa77dd65cfb069dc57a38d9384072db8bf8ba20
Vinayak <moorthyvina@gmail.com>
2019-02-22 16:58:22 +0530

    Created new project for App permission

---
+++ Permissions/Permissions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Permissions
+//
+//  Created by Vinayak on 22/02/19.
+//  Copyright © 2019 Vinayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
